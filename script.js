0f3edb00b8919ab8fef1f1b2b1ec3231afc39905
RedPanda57 <143101177+RedPanda57@users.noreply.github.com>
2024-02-14 19:09:04 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-//    add variables for the code
-//    'const' = can't vary
-//    'var' = varies on what you set it to in the code and can be changed inside of functions
-
-
-// 'Draw' function, this function can call other functions to check 50 times per second if requirements are met.
-function draw() {
-  
-}
